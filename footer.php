5ba4a3f8772e6298234d2dd28b7e524a1f81f642
melinaphan3 <77140751+melinaphan3@users.noreply.github.com>
2021-04-29 17:11:35 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,8 @@
-
+<?php
+  echo "<hr />";
+  echo "<div class=\"footer-copyright\" style=\"color: white\">";
+  echo "<div class=\"container-fluid\">";
+  echo "© 2021 Panther Air Inc.";
+  echo "</div>";
+  echo "</div>";
+?>
